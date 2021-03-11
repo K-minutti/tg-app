aae40087f14f538edfca86e5d05d0393877c6c1a
Kevin Minutti <minutti70@gmail.com>
2021-03-10 19:32:36 -0500

    more files and trying to create constants

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import app1
-import app2
+import home
+import engagement
+import ecosystem
+import community
+import plan
+import settings
+from PIL import Image
 import streamlit as st
 
 st.set_page_config(layout="wide",initial_sidebar_state="collapsed" )
 
 PAGES = {
-    "App1" : app1,
-    "App2" : app2
+    "Home" : home,
+    "Engagement" : engagement, 
+    "Ecosystem": ecosystem,
+    "Community Search": community,
+    "Planning" : plan,
+    "Settings" : settings
+
 }
 
 #selection = st.sidebar.selectbox("NAVIGATION", list(PAGES.keys()))
-
+title = "Home"
 col1, col2 = st.beta_columns([4, 1])
-col1.title("Title of brand")
 selection = col2.selectbox("", list(PAGES.keys()))
+title = selection
+col1.title(title)
+st.write("""***""")
+image = Image.open(settings.IMAGE_LOGO)
+st.sidebar.image(image, use_column_width=True)
+name = settings.NAME
+st.sidebar.subheader(name)
+# then name = settings.NAME
 page = PAGES[selection]
 
 #main container
